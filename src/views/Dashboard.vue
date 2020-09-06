3ed71b506042bb06142a729c5993cab6d2a1e47a
Yasin Tatar <ytatar9@googlemail.com>
2020-09-06 20:32:57 +0200

    init

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Dashboard here</div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  created() {
+    alert("Dashboard created with url:" + window.location);
+    let token = window.location.hash
+      .substr(1)
+      .split("&")[0]
+      .split("=")[1];
+    alert(token);
+  }
+};
+</script>
+
+<style></style>
