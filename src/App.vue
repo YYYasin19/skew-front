1e8d64953f141da7cbc6efb7cee8165114162743
Yasin Tatar <ytatar9@googlemail.com>
2020-09-13 23:26:08 +0200

    added Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Navbar></Navbar>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
@@ -10,8 +11,13 @@
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
+
 export default {
   name: "App",
+  components: {
+    Navbar
+  },
   created() {}
 };
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="navbar">
+    <b-navbar>
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img
+            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+            alt="Lightweight UI components for Vue.js based on Bulma"
+          />
+        </b-navbar-item>
+      </template>
+
+      <template slot="end">
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <div v-if="session">
+              <a class="button is-light">
+                Leave Session
+              </a>
+            </div>
+            <div v-else>
+              <a class="button is-dark">
+                Join a session
+              </a>
+              <a class="button is-primary">
+                Create your own
+              </a>
+            </div>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "Navbar",
+  computed: {
+    ...mapState["session"]
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  color: red;
+}
+</style>
